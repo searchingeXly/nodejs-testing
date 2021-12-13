710f5965146b24ff1249cc90b2dc5669dfb8a55f
searchingeXly <searchingeXly@gmail.com>
2021-12-13 11:24:27 -0500

    Basic middleware

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-const { application } = require("express");
 const express = require("express");
 const app = express();
-const { products } = require("./data");
-app.get("/", (req, res) => {
-  res.send(`<h1>Home Page</h1><a href="/api/products">products</a>`);
+
+// req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
-app.get("/api/products", (req, res) => {
-  const trimmedProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(trimmedProducts);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const { id } = req.params;
-  const singleProduct = products.find((product) => product.id === Number(id));
-  if (!singleProduct) {
-    return res.status(404).send("Product Does Not Exist");
-  }
-  res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("hello");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-  if (sortedProducts.length < 1) {
-    return res.status(200).json({ success: true, data: [] });
-  }
-  return res.status(200).json(sortedProducts);
-});
-
-app.all("*", (req, res) => {
+app.all("*", logger, (req, res) => {
   res.status(404).send("resource not found");
 });
 
