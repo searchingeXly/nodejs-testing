dfa8240e7a086d1371f29faab585761b181a6c05
searchingeXly <searchingeXly@gmail.com>
2021-12-13 15:14:47 -0500

    post,put,delete

--- app.js
+++ app.js
@@ -1,21 +1,78 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
-const logger = require("./logger");
-const authorize = require("./authorize");
+let { people } = require("./data");
 
-// req => middleware => res
-// app.use([logger, authorize]);
-app.use(morgan("tiny"));
+app.use(express.static("./methods-public"));
+//parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Home");
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
-app.get("/about", (req, res) => {
-  res.send("About");
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
-app.get("/api", (req, res) => {
-  res.send("API");
+
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  } else {
+    return res.status(201).json({ success: true, data: [...people, name] });
+  }
+});
+
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` });
+  } else {
+    const newPeople = people.map((person) => {
+      if (person.id === Number(id)) {
+        person.name = name;
+      }
+      return person;
+    });
+    return res.status(200).json({ success: true, data: newPeople });
+  }
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${req.params.id}` });
+  } else {
+    const newPeople = people.filter(
+      (person) => person.id !== Number(req.params.id)
+    );
+    return res.status(200).json({ success: true, data: newPeople });
+  }
+});
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("please provide credentials");
 });
 
 app.all("*", (req, res) => {
