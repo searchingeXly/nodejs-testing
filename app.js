840288d7c8c30deb09ee4a20d95f8fa8b2d3a59f
searchingeXly <searchingeXly@gmail.com>
2021-12-13 19:04:50 -0500

    basic setup with mongoose

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+const tasksRouter = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+app.use("/api/v1/tasks", tasksRouter);
+
+const port = 8000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
