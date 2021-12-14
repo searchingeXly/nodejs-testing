dfb3cf5b08778ada19bcc2e3b08f991d0d6405f2
searchingeXly <searchingeXly@gmail.com>
2021-12-14 00:15:57 -0500

    Basic search queries

--- controllers/products.js
+++ controllers/products.js
@@ -2,11 +2,23 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({ featured: true });
-  res.status(200).json({ nbHits: products.length, products });
+  return res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  const products = await Product.find(queryObject);
+  return res.status(200).json({ nbHits: products.length, products });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
