1d4049bda9b2774f7248e88187faa4bd9f533bb2
searchingeXly <searchingeXly@gmail.com>
2021-12-13 23:52:01 -0500

    basic app setup

---
+++ controllers/products.js
@@ -0,0 +1,12 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ nbHits: products.length, products });
+};
+
+const getAllProducts = async (req, res) => {
+  res.status(200).json({ msg: "products route" });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
